f21597c4c8e536056550ec5ad8900f17fa54766b
Glasgalas <alex.starichenko@icloud.com>
2022-06-24 21:36:00 +0300

    init server

---
+++ server.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = require("./index");
+
+dotenv.config();
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
