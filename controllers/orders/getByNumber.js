e08c1bbaf55eccb9767b21344f22b8e40822bc72
Glasgalas <alex.starichenko@icloud.com>
2022-06-29 23:08:10 +0300

    add error

--- controllers/orders/getByNumber.js
+++ controllers/orders/getByNumber.js
@@ -1,8 +1,12 @@
 const { Order } = require("../../models");
+const { NotFound } = require("http-errors");
 
 const getByNumber = async (req, res) => {
   const { number } = req.body;
   const result = await Order.findOne({ number });
+  if (!result) {
+    throw new NotFound(`Order with number ${number} not found`);
+  }
 
   res.status(200).json({
     status: "success",
