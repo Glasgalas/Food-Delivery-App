1a9a2a7c08a8b7368b0a6a78c7a34e8857ac6a7b
Glasgalas <alex.starichenko@icloud.com>
2023-06-06 23:06:23 +0300

    add vercel.json

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const productsRouter = require("./routes/api/products");
 const usersRouter = require("./routes/api/users");
 const ordersRouter = require("./routes/api/orders");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+// ======swagger
+// const swaggerJSDoc = require("swagger-jsdoc");
+// const swaggerUi = require("swagger-ui-express");
+// const swaggerDefinition = {
+//   openapi: "3.0.0",
+//   info: {
+//     title: "Express API for JSONPlaceholder",
+//     version: "1.0.0",
+//     description:
+//       "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
+//     license: {
+//       name: "Licensed Under MIT",
+//       url: "https://spdx.org/licenses/MIT.html",
+//     },
+//     contact: {
+//       name: "JSONPlaceholder",
+//       url: "https://jsonplaceholder.typicode.com",
+//     },
+//   },
+//   servers: [
+//     {
+//       url: "http://localhost:3030",
+//       description: "Development server",
+//     },
+//   ],
+// };
+// const options = {
+//   swaggerDefinition,
+//   // Paths to files containing OpenAPI definitions
+//   apis: ["./routes/*.js"],
+// };
+// const swaggerSpec = swaggerJSDoc(options);
+// ========
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -13,6 +49,12 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+// =====
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// =====
 
 app.use("/api/products", productsRouter);
 app.use("/api/users", usersRouter);
